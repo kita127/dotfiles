30b3ac0ff3326abf5f80e67fda94a32186f02a5e
kita127 <kitada.kosuke@gmail.com>
2019-03-17 20:48:15 +0900

    xmobar を有効

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,10 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
  
-main = xmonad defaultConfig
+-- =<< xmobar
+-- xmobar の機能を有効にする
+--
+main = xmonad =<< xmobar defaultConfig
         -- Use Alt(mod1Mask) or Super(mod4Mask)
         { modMask = mod1Mask -- Use Super instead of Alt
 
