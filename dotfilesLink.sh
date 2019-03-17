42564bbe033b2ce91dfca2cda379ad0bdcb1bc91
kita127 <kitada.kosuke@gmail.com>
2019-03-17 15:05:12 +0900

    rm dotfilesLink

--- dotfilesLink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.profile ~/.profile
-ln -sf ~/dotfiles/.screenrc ~/.screenrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.my_autokey ~/.my_autokey
-ln -sf ~/dotfiles/xmonad/xmonad.hs ~/.xmonad
