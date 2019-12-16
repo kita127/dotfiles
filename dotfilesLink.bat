9126d0a65b6938fb085e45a9dd7f86bd08cfacf1
kita127 <kitada.kosuke@gmail.com>
2019-12-16 20:01:41 +0900

    update

--- dotfilesLink.bat
+++ dotfilesLink.bat
@@ -1,2 +1,3 @@
 mklink %USERPROFILE%\_vimrc %USERPROFILE%\dotfiles\_vimrc
 mklink %USERPROFILE%\_gvimrc %USERPROFILE%\dotfiles\_gvimrc
+mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\.gitconfig
