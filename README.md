09a9cc431832891788ea44abb7f72f7cfca163d0
北田光祐 <kitada@kitadahikariyuunoMacBook-Pro.local>
2020-08-05 00:18:39 +0900

    update

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## Description
 ドットファイル管理用リポジトリ mac 版
 
+curl -L https://github.com/kita127/dotfiles/mac/install
+
 ### vim
 ### zsh
 ### git
--- install.sh
+++
@@ -1,3 +0,0 @@
-mklink %USERPROFILE%\_vimrc %USERPROFILE%\dotfiles\_vimrc
-mklink %USERPROFILE%\_gvimrc %USERPROFILE%\dotfiles\_gvimrc
-mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\.gitconfig
