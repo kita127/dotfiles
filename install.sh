046f0cb38ca6d35caae5ff53f8cd6ad77fc071ab
kita127 <kitada.kosuke@gmail.com>
2020-08-09 20:46:34 +0900

    add tmux

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
